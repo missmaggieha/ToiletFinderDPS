4e9ea931ac1a9b336dd36281eb61f1445a4fdcbf
missmaggieha <mha2@myseneca.ca>
2014-11-26 16:27:49 -0500

    Testing

--- app/src/main/java/seneca/dps924/toiletfinder/MainActivity.java
+++ app/src/main/java/seneca/dps924/toiletfinder/MainActivity.java
@@ -18,7 +18,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-
+// TESTING 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
