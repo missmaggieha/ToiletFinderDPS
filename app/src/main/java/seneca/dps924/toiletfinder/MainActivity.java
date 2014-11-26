61a9f7dd34a722f63832a168ccea82b4abd693b5
Pedro Bellesa <bspedropaulo@myseneca.ca>
2014-11-26 16:26:12 -0500

    Testing

--- app/src/main/java/seneca/dps924/toiletfinder/MainActivity.java
+++ app/src/main/java/seneca/dps924/toiletfinder/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        String something;
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
